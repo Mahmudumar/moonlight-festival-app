53059e9a3804acd7549653931caca6e66505f2c4
Mahmud <mahmudumar476@gmail.com>
2025-07-27 17:12:25 +0100

    let's make the images....now we have reverted back to yesterddays work. with some package stuff

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/moonlight-festival-app/', // 👈 replace with actual repo name
-  plugins: [react()],
-});
-// https://github.com/Mahmudumar/moonlight-festival-app.git
